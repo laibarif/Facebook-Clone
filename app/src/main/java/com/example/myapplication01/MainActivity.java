6e17cf2936b27373493732342a83a1e17d999f68
Abdullah2021CS198 <abd.sialvi64@gmail.com>
2023-09-28 00:45:43 +0500

    Add Signin

---
+++ app/src/main/java/com/example/myapplication01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication01;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Application;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.Toast;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
